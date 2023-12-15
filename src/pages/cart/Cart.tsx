064f09373a1b4d1636e23a93ada998719e47d7f8
naralicecosta <naralicecosta04@gmail.com>
2023-12-14 22:12:16 -0300

    :zap: feat(listando carrinho):

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,30 +1,49 @@
+import { useContext } from "react"
+import { CartContext } from "../../contexts/CartContext"
+import { Link } from "react-router-dom"
+
 export function Cart(){
+    const {cart} = useContext(CartContext)
     return(
         <div className="w-full max-w 7xl mx-auto">
             <h1 className="font-medium text-2xl text-center my-4">
                 Página carrinho
             </h1>
-            <section className="flex items-center justify-between border-b-2 border-gray-300">
-                <img src="https://m.media-amazon.com/images/I/61CVih3UpdL._AC_SX679_.jpg" alt="logo produto"
+            {cart.length === 0 &&(
+                <div className="flex flex-col items-center justify-center">
+                    <p className="font-medium">Ops! seu carrinho esta vazio ":(" </p>
+               
+                    <Link to="/" className="bg-slate-600 my-3 p-1 px-3 text-white font-medium rounded">
+                        Acessar Produtos
+                    </Link> 
+                </div>
+            )}
+
+            {cart.map((item) => (
+                <section key={item.id} className="flex items-center justify-between border-b-2 border-gray-300">
+                <img src={item.cover} alt={item.title}
                 className="w-28" />
-                <strong>Preço R$1.000</strong>
+                <strong>Preço {item.price}</strong>
                 
                 <div className="flex items-center justify-center gap-3"> 
                     <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
                         -
                     </button>
-                    2
+                    {item.amount} {/**quantidade */}
                     <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
                         +
                     </button>
                 </div>
                 <strong className='float-right'>
-                    Subtotal: R$10000
+                    Subtotal: {item.total.toLocaleString("pt-BR",{
+                        style: "currency",
+                        currency: "BRL"
+                    })}
                 </strong>
-
-                <p className="font-bold mt-4">Total: R$: 1.000</p>
-
             </section>
+            ))}
+
+            {cart.length !== 0  && <p className="font-bold mt-4">Total: R$: 1.000</p> }
         </div>
     )
 }
